026f6f9696a56d7bc1533c7561d1a07e557ff036
Olha Krapivina <olga.krapivina.zireael@gmail.com>
2022-06-05 12:46:28 +0300

    commit #1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import Page from "./Page";
+import Footer from "./Footer";
 
 function App() {
+  const showAlert = () => {
+    alert("hi");
+  };
+
+  const users = ["V", 'Johnny'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onClick={showAlert}>
+      <Header />
+      <Page users={users} title="My first page" description="My first description" />
+      <Footer />
     </div>
   );
 }
---
+++ src/Page.js
@@ -0,0 +1,13 @@
+function Page({ title }) {
+    console.log(title);
+    return (
+        <div>
+            <h1>{title}</h1>
+            <p>
+                we'll never fade away
+            </p>
+        </div>
+    );
+  }
+  
+  export default Page;
