2d2601742bacd271bd4083cb8f0c20495928eba7
Olha Krapivina <olga.krapivina.zireael@gmail.com>
2022-06-13 13:44:05 +0300

    Added Popup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import posts from "./posts.json";
 import PostList from "./components/PostList";
 import Registration from "./components/Regisration";
+import Popup from "./components/Popup";
 
 function App() {
   return (
     <div>
-      <Registration />
+      <Popup buttonName="Add post" title="Enter post data">
+        <Registration />
+      </Popup>
       {/* <PostList posts={posts} /> */}
     </div>
   );
--- src/components/Regisration.js
+++ src/components/Regisration.js
@@ -1,96 +1,104 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
-import { useState } from 'react';
+import { css } from "@emotion/react";
+import { useState } from "react";
 
 function Regisration() {
-    const [userName, setUserName] = useState("");
-    
-    const onChangeUserName = function(event) {
-        // event.preventDefault();
+  const [userName, setUserName] = useState("");
 
-        setUserName(event.target.value);
+  const onChangeUserName = function (event) {
+    // event.preventDefault();
 
-        // const data = {
-        //     name: event.target.username.value,
-        //     phone: event.target.phone.value,
-        //     email: event.target.email.value,
-        //     city: event.target.city.value,
-        //     subscibe: event.target.subscibe.value,
-        //     gender: event.target.gender.value
-        // };
+    setUserName(event.target.value);
 
-        // fetch("https://google.com", {
-        //     method: "POST",
-        //     body: JSON.stringify(data),
-        // });
-    };
+    // const data = {
+    //     name: event.target.username.value,
+    //     phone: event.target.phone.value,
+    //     email: event.target.email.value,
+    //     city: event.target.city.value,
+    //     subscibe: event.target.subscibe.value,
+    //     gender: event.target.gender.value
+    // };
 
-    return (
-        <form
-            css={css `
-                max-width: 300px;
-                box-shadow: 0 0 5px 2px #0000004a;
-                margin: 100px auto;
-                padding: 10px;
+    // fetch("https://google.com", {
+    //     method: "POST",
+    //     body: JSON.stringify(data),
+    // });
+  };
 
-                input,
-                select {
-                    width: 100%;
-                    box-sizing: border-box;
-                    margin-bottom: 10px;
-                }
-            `}>
-            <div>USER VALUE: {userName}</div>
+  return (
+    <form
+      css={css`
+        max-width: 300px;
+        box-shadow: 0 0 5px 2px #0000004a;
+        padding: 10px;
 
-            <input onChange={onChangeUserName} name="username" type="text" placeholder="Enter user name:" value={userName} />
-            <input name="phone" type="text" placeholder="Enter user phone:" />
-            <input name="email" type="text" placeholder="Enter user email:" />
+        input,
+        select {
+          width: 100%;
+          box-sizing: border-box;
+          margin-bottom: 10px;
+        }
+      `}
+    >
+      <div>USER VALUE: {userName}</div>
 
-            <select name="city">
-                <option>Select city</option>
-                <option>Kyiv</option>
-                <option>Dnipro</option>
-                <option>Odessa</option>
-                <option>Kharkiv</option>
-                <option>Lviv</option>
-            </select>
+      <input
+        onChange={onChangeUserName}
+        name="username"
+        type="text"
+        placeholder="Enter user name:"
+        value={userName}
+      />
+      <input name="phone" type="text" placeholder="Enter user phone:" />
+      <input name="email" type="text" placeholder="Enter user email:" />
 
-            <label
-                css={css `
-                display: flex;
-                width: 100px;
-            `}>
-                Subscibe
-                <input name="subscibe" type="checkbox" />
-            </label>
+      <select name="city">
+        <option>Select city</option>
+        <option>Kyiv</option>
+        <option>Dnipro</option>
+        <option>Odessa</option>
+        <option>Kharkiv</option>
+        <option>Lviv</option>
+      </select>
 
-            <div 
-                css={css `
-                    display: flex;
-                    margin-bottom: 15px;
+      <label
+        css={css`
+          display: flex;
+          width: 100px;
+        `}
+      >
+        Subscibe
+        <input name="subscibe" type="checkbox" />
+      </label>
 
-                    label {
-                        display: flex;
-                        align-items: center;
-                        width: 70px;
-                    }
+      <div
+        css={css`
+          display: flex;
+          margin-bottom: 15px;
 
-                    input {
-                        mangin: 0;
-                    }
-                `}>
-                <label>
-                    Male
-                    <input name="gender" value="male" type="radio" />
-                </label>
-                <label>
-                    Female
-                    <input name="gender" value="female" type="radio" />
-                </label>
-            </div>
+          label {
+            display: flex;
+            align-items: center;
+            width: 70px;
+          }
 
-            <button>Confirm</button>
-        </form>
-    );
+          input {
+            mangin: 0;
+          }
+        `}
+      >
+        <label>
+          Male
+          <input name="gender" value="male" type="radio" />
+        </label>
+        <label>
+          Female
+          <input name="gender" value="female" type="radio" />
+        </label>
+      </div>
+
+      <button>Confirm</button>
+    </form>
+  );
 }
 export default Regisration;
